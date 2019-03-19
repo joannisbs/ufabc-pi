c10cbe498ce52d110b0cbc24eddab84eefa17a50
joannisbs <joannisbs@gmail.com>
2019-03-18 21:21:22 -0300

    2019-03-18

---
+++ aulas/aula03_18.java
@@ -0,0 +1,8 @@
+
+// DESAFIO
+// faça um programa java que leia do usuario um valor real 
+// que separe a parte inteira da parte decimal
+
+// voce nao deve usar em hipotese aluma conversao de tipos. 
+
+// ferramentas soma subtração e repecicoes
---
+++ exercícios/extrair.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class extrair {
+  public static void main(String args[]) {
+    Scanner scanner = new Scanner(System.in);
+
+    float dadoLido = scanner.nextFloat();
+
+    int parteInteira = 0;
+    while ( dadoLido > 0 ) {
+        dadoLido = dadoLido - 1;
+        parteInteira = parteInteira + 1;
+    }
+
+    System.out.print("parteInteira: ");
+    System.out.println(parteInteira);
+
+    System.out.print("parteDecimaql: ");
+    System.out.println(dadoLido+1);
+  }
+}
